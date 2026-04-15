1fc91d6ec418bdc750e905b65c7e4037358eb253
dimsrist <dimasristanto1@gmail.com>
2026-04-15 09:19:46 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
-int main(void) {
+int hitungLangkah(int n) {
+    if (n == 1) {
+        return 0;
+    }
+    
+    else if (n % 2 == 0) {
+        return 1 + hitungLangkah(n / 2);
+    } 
+    else {
+        return 1 + hitungLangkah(3 * n + 1);
+    }
+}
+
+int main() {
+    int T;
+    scanf(" %d", &T);
+    int n;
+    for (int i=0;i<T; i++) {
+        scanf("%d", &n);
+        if (n <= 0) {
+        printf("Masukkan bilangan bulat positif lebih dari 0.\n");
+        } else {
+            int langkah = hitungLangkah(n);
+            printf("Langkah %d\n", langkah);
+        }
+    }
     return 0;
 }
